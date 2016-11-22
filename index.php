d76fee40ef544c3b755173223ec763c17444cf6b
Benjamin Doherty <bjd@bangpound.org>
2016-11-21 23:08:17 -0600

    pass variables through app constructor.

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$app = new Stylex\Application(['debug' => TRUE]);
+$app = new Stylex\Application([
+  'templates' => 'templates',
+  'data' => 'data',
+  'content' => 'content',
+  'debug' => TRUE,
+]);
 $app->run();
